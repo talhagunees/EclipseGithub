dabdb3234276c71c377c36d5bba7a389fd030387
İbrahim KARAKUŞ <brahim KARAKU@192.168.1.13>
2022-09-27 15:59:18 +0300

    FirstCommit

---
+++ src/introo/main1.java
@@ -0,0 +1,60 @@
+package introo;
+
+public class main1 {
+	
+	public static void main(String[] args) {
+		System.out.println("HELLO WORLD!");
+		
+		//değişken isimlendirmeleri Java'da camelCase yazılır
+		//String metinsel veri
+		//Her kod ; ile biter
+		String ortaMetin = "İlginizi Çekebilir";
+		String altMetin = "Vade Süresi";
+		
+		//Referans verdiğin değişkeni çift tırnak olmadan yaz
+		System.out.println(altMetin);
+		
+		//integer tam sayı değişkeni
+		int vade = 12;
+		
+		System.out.println(vade);
+	
+		// double ondalıklı sayı değişkeni
+		double dolarDun = 18.25;
+		double dolarBugun = 18.30;
+		
+		//şartlı bloklar mantıksal veri
+		//true veya false sorusuna cevap verebilcek
+		boolean dolarDustuMu = false;
+		
+		String okYonu = "";
+		
+		//if satırının içindeki kod sağlanırsa kendi bloğunu çalıştırır
+		// if else mutlaka çalışır ama sadece biri çalışır
+		// if else satırlarının sonuna ; gelmez
+		if (dolarBugun<dolarDun) {
+			okYonu = "down.svg";	
+			System.out.println(okYonu);
+			
+		} else if(dolarBugun>dolarDun) {
+			okYonu = "up.svg";
+			System.out.println(okYonu);
+		}
+		else {	
+			okYonu = "equal.svg";
+			System.out.println(okYonu);
+			
+		}
+		//array	
+		//köşeli parantez görürsen o bir liste
+		String[] krediler = {"Hızlı Kredi" , "Maaşını Halkbanktan" , "Mutlu Emekli"};
+		
+        //kodlamada saymaya 0'dan başlanır
+		//for=dögü, bir şeyi bir şarta göre tekrarla
+		for (int i = 0; i < krediler.length; i++) {
+			System.out.println(krediler[i]);
+		}
+	}
+
+}
+
---
+++ src/introo/recapDemo1.java
@@ -0,0 +1,21 @@
+package introo;
+
+public class recapDemo1 {
+
+	public static void main(String[] args) {
+		int sayı1 = 224;
+		int sayı2 = 25;
+		int sayı3 = 26;
+		int enBuyuk = sayı1;
+		
+		if(enBuyuk<sayı2) {
+		     enBuyuk = sayı2;
+			} 
+		if (enBuyuk<sayı3) {
+			enBuyuk = sayı3;
+				}
+		System.out.println(enBuyuk); 	
+
+	}
+
+}
